121d7a2a1c7494549f9ed08359805f5debb76679
JuanS1lva <juan.silva.vane@gmail.com>
2020-09-19 01:40:58 -0500

    Ejercicios taller 1

---
+++ ej2.js
@@ -0,0 +1,5 @@
+let temp = prompt('Ingrese una frase');
+let espacios=temp.split(" ").length-1;
+let caracteres = temp.length-espacios;
+alert("Hay "+espacios+" espacios y "+caracteres+ " caracteres");
+
---
+++ ej3.js
@@ -0,0 +1,14 @@
+let temp = prompt('Ingrese una frase');
+let espacios=temp.split("");
+for(let i =0; i<espacios.length;i++){
+  if (espacios[i] != " ") {
+    espacios[i]=espacios[i]+"-"
+  } 
+  else {
+    espacios[i]="--"
+  }
+}
+temp=espacios.join('');
+temp= temp.slice(0,-1);
+alert(temp);
+
